072260fe620a6fc5b2104c74996b50f4b7da802d
Miki <simsoneditor@gmail.com>
2022-03-01 17:25:30 +0100

    mainwindow

---
+++ sztf4/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using sztf4.Model;
+
+namespace sztf4.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string s = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(s));
+        }
+
+        public BindingList<SuperHero> superHeroes;
+        public BindingList<SuperHero> selectedSuperHeroes;
+        public SuperHero selectedHero;
+
+
+        public MainWindowViewModel()
+        {
+            superHeroes = new BindingList<SuperHero>();
+            selectedSuperHeroes = new BindingList<SuperHero>();
+
+            superHeroes.Add(new SuperHero(){ Name = "SuperMan", Speed = 8, Strength = 8, Role = 0 }); 
+        }
+
+
+
+
+    }
+}
