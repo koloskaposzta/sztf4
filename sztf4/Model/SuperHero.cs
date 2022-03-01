15ed66a03902eaf3b216a0cab9143c17642a9e2d
hallgato <hallgato@LABOR215-15>
2022-03-01 16:35:39 +0100

    model init

---
+++ sztf4/Model/SuperHero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sztf4.Model
+{
+  public  class SuperHero
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Speed { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
